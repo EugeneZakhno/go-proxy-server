814d1e91b4f5bf657dac9bec36799d7fa7e2fd27
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-12-09 11:40:39 +0300

    change url01

--- proxy.go
+++ proxy.go
@@ -11,7 +11,7 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	client := &http.Client{}
 
 	// Задаем целевой URL (например, http://example.com)
-	targetURL := "https://openapi.mypay.kz" + r.URL.Path // Прокси на другой сервер
+	targetURL := "https://openapi.mypay.kz/api/v4/ABRIVTIPS6C38D03FD1DAD0F42F47F0A/payment/MYO6QPZ3IWZGI5LW" + r.URL.Path // Прокси на другой сервер
 
 	// Создаем новый запрос с теми же методами и заголовками
 	req, err := http.NewRequest(r.Method, targetURL, nil) // nil для GET-запроса
